870053c4df4ec108639d3e8b3a6c05ad59f7fcb0
kiyongname <stellis001@gmail.com>
2021-03-08 23:46:12 +0900

    리스트 뷰 템플릿까지 210308

--- photo/models.py
+++ photo/models.py
@@ -17,8 +17,10 @@ class Photo(models.Model):
     class Meta:
         ordering = ['-updated']
 
+    #작성자의 이름과 글 작성일을 합친 문자열 반환
     def __str__(self):
         return self.author.username + " " + self.created.strftime("%Y-%m-%d-%H:%M:%S")
 
+    #객체의 상세 페이지의 주소를 반환
     def get_absolute_url(self):
         return reverse('photo:photo_detail', args=[str(self.id)])
